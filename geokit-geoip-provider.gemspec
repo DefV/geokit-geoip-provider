0c65641bd56e0a3c961b7b92d7429c390377689d
Jan De Poorter <jan@defv.be>
2009-08-21 17:17:55 +0200

    Initial commit

---
+++ geokit-geoip-provider.gemspec
@@ -0,0 +1,15 @@
+Gem::Specification.new do |s|
+    s.platform  =   Gem::Platform::RUBY
+    s.name      =   "geokit-geoip-provider"
+    s.version   =   "0.1"
+    s.author    =   "Jan De Poorter"
+    s.email     =   "geokit@defv.be"
+    s.homepage   =   "http://workswithruby.com"
+    s.summary   =   "GeoIP provider for geokit gem"
+    s.files     =   %w(README lib/geokit-geoip-provider.rb)
+    s.require_path  =   "lib"
+    s.has_rdoc  =   false
+    s.extra_rdoc_files  = ["README"]
+end
+
+
---
+++ lib/geokit-geoip-provider.rb
@@ -0,0 +1,32 @@
+require 'geokit'
+require 'geoip'
+
+module Geokit
+  module Geocoders
+
+    # Should be overriden as Geokit::Geocoders::external_key in your configuration file
+    @@geo_ip_database = 'REPLACE_WITH_GEOCITY_LOCATION'
+    __define_accessors
+
+    class GeoIpGeocoder < Geocoder
+      private
+      def self.db
+        @@db ||= GeoIP.new(Geocoders.geo_ip_database)  
+      end
+      
+      def self.do_geocode(address, options = {})
+        ip, ip2, country_code, country_code2, country, continent, region, city, postal, latitude, longitude, usa = db.city(address)
+        
+        res = GeoLoc.new
+        res.city = city
+        res.country_code = country_code
+        res.zip = postal
+        res.lat = latitude
+        res.lng = longitude
+        
+        res
+      end
+    end
+
+  end
+end
